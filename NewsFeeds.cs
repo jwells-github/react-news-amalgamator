4d185cce71f240a40de7925a175eda8b7b087625
Jake <jakewellsd@gmail.com>
2021-07-19 22:18:12 +0100

    Add NewsFeeds file

---
+++ NewsFeeds.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace react_news_app
+{
+    public static class NewsFeeds
+    {
+        public const string THE_GUARDIAN_FEED = "https://www.theguardian.com/world/rss";
+        public const string BBC_NEWS_FEED = "http://feeds.bbci.co.uk/news/rss.xml";
+        public const string DAILY_MAIL_FEED = "https://www.dailymail.co.uk/news/index.rss";
+        public const string THE_TELEGRAPH_FEED = "https://www.telegraph.co.uk/news/rss.xml";
+    }
+}
