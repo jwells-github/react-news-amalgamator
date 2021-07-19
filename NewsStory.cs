a4f7ed6538e460da7f2faec4ee0866e8da6f8522
Jake <jakewellsd@gmail.com>
2021-07-19 22:18:52 +0100

    Add Provider enum and a method to return a provider based on a given url

--- NewsStory.cs
+++ NewsStory.cs
@@ -9,5 +9,34 @@ namespace react_news_app
         public string StoryUrl { get; set; }
         public DateTime Date { get; set; }
         public string ImageUrl { get; set; }
+        public Provider Provider { get; set; }
+
+        public static Provider getProviderFromFeed(string feed)
+        {
+
+            switch (feed)
+            {
+                case NewsFeeds.THE_GUARDIAN_FEED:
+                    return Provider.THE_GUARDIAN;
+                case NewsFeeds.BBC_NEWS_FEED:
+                    return Provider.BBC_NEWS;
+                case NewsFeeds.DAILY_MAIL_FEED:
+                    return Provider.DAILY_MAIL;
+                case NewsFeeds.THE_TELEGRAPH_FEED:
+                    return Provider.THE_TELEGRAPH;
+                default:
+                    return Provider.NOT_SET;
+
+            }
+        }
+    }   
+
+    public enum Provider
+    {
+        NOT_SET = 0,
+        THE_GUARDIAN = 1,
+        BBC_NEWS = 2,
+        DAILY_MAIL = 3,
+        THE_TELEGRAPH = 4
     }
 }
