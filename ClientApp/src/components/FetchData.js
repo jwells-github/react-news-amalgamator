54f8fc08ed73abea73b6d6b3a8436146fc7a6932
Jake <jakewellsd@gmail.com>
2021-07-21 16:01:52 +0100

    Display the provider for each story

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -3,20 +3,21 @@ import React, { Component } from 'react';
 export class FetchData extends Component {
     static displayName = FetchData.name;
     static Provider = {
-        NOT_SET: 0,
-        THE_GUARDIAN: 1,
-        BBC_NEWS: 2,
-        DAILY_MAIL: 3,
-        THE_TELEGRAPH: 4
+        NOT_SET: {id:0, name: ""},
+        THE_GUARDIAN: { id: 1, name: "The Guardian" },
+        BBC_NEWS: { id: 2, name: "BBC News" },
+        DAILY_MAIL: { id: 3, name: "Daily Mail Online" },
+        THE_TELEGRAPH: { id: 4, name: "The Telegraph" }
     };
-  constructor(props) {
-    super(props);
-      this.state = { forecasts: [], loading: true, provider: 1, filteredStories:[] };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { forecasts: [], loading: true, provider: 1, filteredStories:[] };
+    }
 
-  componentDidMount() {
-    this.populateWeatherData();
-    }   
+    componentDidMount() {
+        this.populateWeatherData();
+    }
+
     static renderStories(amalgamatedStories) {
         return (
             <div>
@@ -28,14 +29,14 @@ export class FetchData extends Component {
                             </div>
                             <div>
                                 <h2>
-                                    <a href={amalgamatedStory.mainStory.storyUrl}>{amalgamatedStory.mainStory.title}</a>
+                                    { amalgamatedStory.mainStory.providerName } - <a href={amalgamatedStory.mainStory.storyUrl}>{amalgamatedStory.mainStory.title}</a>
                                 </h2>
                                 <p dangerouslySetInnerHTML={{ __html: amalgamatedStory.mainStory.description }}></p>
                             </div>
                         </div>
                         <div>
-                            {amalgamatedStory.childStories.map(story =>
-                                <a href={story.storyUrl}>{story.title}<br></br></a>
+                            {amalgamatedStory.childStories.map(story => 
+                                <span> {story.providerName} -  <a href={story.storyUrl}>{story.title}</a><br></br></span>
                             )}
                         </div>
                     </div>
@@ -58,27 +59,32 @@ export class FetchData extends Component {
     );
   }
 
-  async populateWeatherData() {
-    const response = await fetch('stories');
-    const data = await response.json();
-    let filteredStories = [];
-    data.map(amalgamatedStory => {
-        let mainStory;
-        let childStories = [];
-        amalgamatedStory.stories.map(story => {
-            if (story.provider === this.state.provider) {
-                mainStory = story
+    async populateWeatherData() {
+        const response = await fetch('stories');
+        const data = await response.json();
+        this.setState({ forecasts: data });
+        this.applyProviderPreference(data)     
+    }
+
+    applyProviderPreference(data) {
+        let filteredStories = [];
+        data.map(amalgamatedStory => {
+            let mainStory;
+            let childStories = [];
+            amalgamatedStory.stories.map(story => {
+                if (story.provider === this.state.provider) {
+                    mainStory = story
+                }
+                else {
+                    childStories.push(story)
+                }
+            })
+            if (mainStory === undefined) {
+                mainStory = childStories[0];
+                childStories.shift();
             }
-            else {
-                childStories.push(story)
-            }
+            filteredStories.push({ mainStory: mainStory, childStories: childStories });
         })
-        if (mainStory === undefined) {
-            mainStory = childStories[0];
-            childStories.shift();
-        }
-        filteredStories.push({ mainStory: mainStory, childStories: childStories });
-    })
-    this.setState({ forecasts: data, loading: false, filteredStories: filteredStories });
-  }
+        this.setState({ forecasts: data, loading: false, filteredStories: filteredStories });
+    }
 }
