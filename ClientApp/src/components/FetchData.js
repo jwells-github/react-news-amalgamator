89be9e9fdd170fb03812d34a883e2d789e2f60d6
Jake <jakewellsd@gmail.com>
2021-08-05 16:01:01 +0100

    Add user interface to toggle displayed news providers

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -3,23 +3,23 @@ import { AmalgamatedStory } from './AmalgamatedStory';
 
 export class FetchData extends Component {
     static displayName = FetchData.name;
-    static Provider = {
-        THE_GUARDIAN: { id: 1, name: "The Guardian" },
-        BBC_NEWS: { id: 2, name: "BBC News" },
-        DAILY_MAIL: { id: 3, name: "Daily Mail Online" },
-        THE_TELEGRAPH: { id: 4, name: "The Telegraph" }
-    };
+
     constructor(props) {
         super(props);
         this.state = {
             storyData: [],
             loading: true,
-            provider: FetchData.Provider.THE_GUARDIAN.id,
-            bannedProviders: [],
-            filteredStories: []
-
+            provider: 0,
+            filteredStories: [],
+            providers: {
+                THE_GUARDIAN: { id: 1, name: "The Guardian", display: true },
+                BBC_NEWS: { id: 2, name: "BBC News", display: true },
+                DAILY_MAIL: { id: 3, name: "Daily Mail Online", display: true },
+                THE_TELEGRAPH: { id: 4, name: "The Telegraph", display: true }
+            }
         };
         this.changeProviderPreference = this.changeProviderPreference.bind(this);
+        this.toggleDisplayedProviders = this.toggleDisplayedProviders.bind(this);
     }
 
     componentDidMount() {
@@ -42,8 +42,18 @@ export class FetchData extends Component {
         )
     }
 
-
+    toggleDisplayedProviders(e) {
+        const target = e.target;
+        const checked = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        let providers = this.state.providers;
+        providers[name].display = checked;
+        this.setState({ providers: providers },
+            this.applyProviderPreference
+        )
+    }
     changeProviderPreference(e) {
+        console.log('why')
         this.setState({ provider: parseInt(e.target.value) },
             this.applyProviderPreference
         );
@@ -53,19 +63,36 @@ export class FetchData extends Component {
         let contents = this.state.loading
             ? <p><em>Loading...</em></p>
             : FetchData.renderStories(this.state.filteredStories);
-        let providers = Object.values(FetchData.Provider)
+        let providerList = [];
+        for (const [key, provider] of Object.entries(this.state.providers)) {
+            providerList.push(
+                <div key={provider.id}>
+                    <input
+                        name={key}
+                        type="checkbox" id={provider.name}
+                        checked={provider.display}
+                        onChange={this.toggleDisplayedProviders} />
+                    <label htmlFor={provider.name}>{provider.name}</label>
+                </div>)
+        }
 
         return (
             <div>
                 <h1>Weather forecast</h1>
+                <h2>Preffered news provider</h2>
                 <select value={this.state.provider} onChange={this.changeProviderPreference}>
-                    {providers.map(provider =>
+                    <option key={0} value={0}>No preference</option>
+                    {Object.values(this.state.providers).map(provider =>
                         <option key={provider.id} value={provider.id}>{provider.name}</option>
                     )}
                 </select>
+                <div>
+                    <h2>News providers to display</h2>
+                    {providerList}
+                </div>
                 {contents}
             </div>
-        );
+        )
     }
 
     async populateWeatherData() {
@@ -81,12 +108,21 @@ export class FetchData extends Component {
     applyProviderPreference() {
         this.setState({loading: true})
         let filteredStories = [];
-        this.state.storyData.map(amalgamatedStory => {
+        this.state.storyData.forEach(amalgamatedStory => {
             let mainStory;
             let childStories = [];
-            let chosenProviders = amalgamatedStory.stories.filter(story => !this.state.bannedProviders.includes(story.provider));
+            let chosenProviders = amalgamatedStory.stories.filter(story => {
+                let displayStory = true;;
+                let providers = Object.values(this.state.providers)
+                providers.forEach(provider => {
+                    if (provider.id === story.provider) {
+                        displayStory = provider.display;
+                    }
+                })
+                return displayStory;
+            });
             let storiesAvailable = chosenProviders.length > 0;
-            chosenProviders.map(story => {
+            chosenProviders.forEach(story => {
                 if (story.provider === this.state.provider) {
                     mainStory = story
                 }
@@ -94,14 +130,14 @@ export class FetchData extends Component {
                     childStories.push(story)
                 }
             })
-            if (mainStory === undefined) { 
+            if (mainStory === undefined) {
                 mainStory = childStories[0];
                 childStories.shift();
             }
             if (storiesAvailable) {
                 filteredStories.push({ mainStory: mainStory, childStories: childStories });
             }
-           
+
         })
         this.setState({ loading: false, filteredStories: filteredStories });
     }
