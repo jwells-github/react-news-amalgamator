9d41b4ce9107e4011b355d029aec2269450d0840
Jake <jakewellsd@gmail.com>
2021-08-05 16:01:13 +0100

    Remove unnecessary constructor

--- ClientApp/src/components/FormattedDate.js
+++ ClientApp/src/components/FormattedDate.js
@@ -1,12 +1,6 @@
 ﻿import React, { Component } from 'react';
 
 export class FormattedDate extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-
     render() {
         return (
             <span>{new Date(Date.parse(this.props.date)).toString().substr(0, 21)}</span>
