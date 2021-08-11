6883d0e84068c678b194b871777c52659bdb0f6e
Jake <jakewellsd@gmail.com>
2021-08-11 16:33:41 +0100

    Add button to view options on navbar

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -32,7 +32,7 @@ export class NavMenu extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                    <button onClick={this.props.toggleOptionsDisplay}>View Options</button>
                 </NavItem>
               </ul>
             </Collapse>
