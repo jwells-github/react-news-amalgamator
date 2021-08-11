dbac052d04d36675390b75cc633a85fd1fb0121c
Jake <jakewellsd@gmail.com>
2021-08-11 13:59:00 +0100

    Split options into Options and a more generic OptionsTab

---
+++ ClientApp/src/components/Options.js
@@ -0,0 +1,50 @@
+﻿import React, { Component } from 'react';
+
+export class Options extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let providerList = [];
+        for (const [key, provider] of Object.entries(this.props.providers)) {
+            providerList.push(
+                <div className="providerSelection" key={provider.id}>
+                    <label htmlFor={provider.name}>{provider.name}</label>
+                    <input
+                        name={key}
+                        type="checkbox" id={provider.name}
+                        checked={provider.display}
+                        onChange={this.props.toggleDisplayedProviders} />
+
+                </div>)
+        }
+        return (
+            <div>
+                <h3>Preferred news provider</h3>
+                <select value={this.props.preferredProvider} onChange={this.props.changeProviderPreference}>
+                    <option key={0} value={0}>No preference</option>
+                    {Object.values(this.props.providers).map(provider =>
+                        <option key={provider.id} value={provider.id}>{provider.name}</option>
+                    )}
+                </select>
+                <div>
+                    <h3>News providers to display</h3>
+                    {providerList}
+                </div>
+                <div>
+                    <h3>Dark Mode</h3>
+                    <div className="providerSelection">
+                        <label htmlFor="toggleDarkMode">Enable dark mode</label>
+                        <input
+                            name="toggleDarkMode"
+                            type="checkbox" id="toggleDarkMode"
+                            checked={this.props.darkModeEnabled}
+                            onChange={this.props.toggleDarkMode} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ ClientApp/src/components/OptionsTab.js
@@ -0,0 +1,31 @@
+﻿import React, { Component } from 'react';
+
+
+export class OptionsTab extends Component {
+
+    constructor(props) {
+        super(props);
+        this.toggleOptionsTab = this.toggleOptionsTab.bind(this);
+        this.optionsTab = React.createRef()
+    }
+
+    componentDidUpdate(prevProps) {
+        // This ensures hidden class is applied on start to prevent the slide animation from running on page load
+        if (prevProps.displayOptions !== this.props.displayOptions) {
+            this.toggleOptionsTab();
+        }
+    }
+
+    toggleOptionsTab() {
+        this.optionsTab.current.className = this.props.displayOptions ? "options-bar show-options" : "options-bar hide-options"
+    }
+
+    render() {
+        return (
+            <div className="hidden" ref={this.optionsTab}>
+                <h2>Options</h2>
+                {this.props.children}
+            </div>
+        );
+    }
+}
